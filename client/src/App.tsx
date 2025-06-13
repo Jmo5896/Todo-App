45bbc344f61a9b688361ecfe229edc07b2be476e
jmo5896 <justin.m.moore5896@gmail.com>
2025-06-13 16:51:43 -0500

    adds some basic navigation functionality to the front end and lays the ground work for graphql

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,41 +1,41 @@
-// import {
-//   ApolloClient,
-//   InMemoryCache,
-//   ApolloProvider,
-//   createHttpLink,
-// } from '@apollo/client';
-// import { setContext } from '@apollo/client/link/context';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 
 import Header from './components/Header';
 
-// const httpLink = createHttpLink({
-//   uri: '/graphql',
-// });
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
-// const authLink = setContext((_, { headers }) => {
+const authLink = setContext((_, { headers }) => {
 
-//   const token = localStorage.getItem('id_token');
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : '',
-//     },
-//   };
-// });
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
-// const client = new ApolloClient({
-//   link: authLink.concat(httpLink),
-//   cache: new InMemoryCache(),
-// });
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-      {/* <ApolloProvider client={client}> */}
-      <Header />
-      <Outlet />
-      {/* </ApolloProvider> */}
+      <ApolloProvider client={client}>
+        <Header />
+        <Outlet />
+      </ApolloProvider>
     </>
   );
 }
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,18 @@
+import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
+
 export default function Header() {
     return (
-        <div>Header</div>
-    )
+        <Nav variant="tabs" defaultActiveKey="/">
+            <Nav.Item>
+                <Nav.Link as={Link} to="/" >Profile</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link as={Link} to="/login" eventKey="/login">Login</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link as={Link} to="/signup" eventKey="/signup">Sign Up</Nav.Link>
+            </Nav.Item>
+        </Nav>
+    );
 }
